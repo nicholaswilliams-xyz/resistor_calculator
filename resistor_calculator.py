bc2d906bdcd33dad51d3f6eec7142d7069066ec6
nicholaswilliams-xyz <109601511+nicholaswilliams-xyz@users.noreply.github.com>
2022-12-13 22:41:21 +1000

    Build app

---
+++ resistor_calculator.py
@@ -0,0 +1,33 @@
+"""
+Calculate the resistance and tolerances of a resistor based on the chosen colour bands.
+Currently works with Python interpreter 3.10
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ListProperty
+from kivy.properties import StringProperty
+
+__author__ = 'Nicholas Williams'
+
+RESISTOR_3_VALUES = (1, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2, 9.1, 10)
+
+
+class ResistorCalculatorApp(App):
+    """ResistorCalculatorApp is a Kivy app for calculating the resistance and tolerance of a
+    resistor based on the chosen colour bands"""
+    resistor_3_values = ListProperty()
+    current_resistor_3_value = StringProperty()
+
+    def build(self):
+        """Build the Kivy ap from the kv file"""
+        self.title = "Resistor calculator 1.0"
+        self.root = Builder.load_file('resistor_calculator.kv')
+        self.resistor_3_values = RESISTOR_3_VALUES
+        self.current_resistor_3_value = self.resistor_3_values[0]
+        return self.root
+
+    def calculate_resistance(self, res):
+        return True
+
+    def display_resistor_colours(self, resistance):
