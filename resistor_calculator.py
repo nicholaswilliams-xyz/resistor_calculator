98836d17a7df5678098d0c3e8e8aa7d1fd18e2aa
nicholaswilliams-xyz <109601511+nicholaswilliams-xyz@users.noreply.github.com>
2022-12-21 21:50:30 +1000

    Add COLOUR_TO_RGBA dictionary and return_rgba() function to change colour of spinners

--- resistor_calculator.py
+++ resistor_calculator.py
@@ -13,23 +13,44 @@ __author__ = 'Nicholas Williams'
 
 RESISTOR_3_VALUES = ("1", "2.2", "2.7", "3.3", "3.9", "4.7", "5.6", "6.8", "8.2", "9.1", "10")
 
+COLOUR_TO_RGBA = {"Black": (0, 0, 0, 1),
+                  "Brown": (165 / 255, 42 / 255, 42 / 255, 1),
+                  "Red": (1, 0, 0, 1),
+                  "Orange": (1, 165 / 255, 0, 1),
+                  "Yellow": (1, 1, 0, 1),
+                  "Green": (0, 1, 0, 1),
+                  "Blue": (0, 0, 1, 1),
+                  "Violet": (128 / 255, 0, 128 / 255, 1),
+                  "Grey": (211 / 255, 211 / 255, 211 / 255, 1),
+                  "White": (1, 1, 1, 1),
+                  "Gold": (1, 215 / 255, 0, 1),
+                  "Silver": (192 / 255, 192 / 255, 192 / 255, 1)}
+
 
 class ResistorCalculatorApp(App):
     """ResistorCalculatorApp is a Kivy app for calculating the resistance and tolerance of a
     resistor based on the chosen colour bands"""
     resistor_3_values = ListProperty()
-    current_resistor_3_value = StringProperty()
+    colour_to_rgba = ListProperty()
 
     def build(self):
         """Build the Kivy app from the kv file"""
         self.title = "Resistor calculator 1.0"
         self.root = Builder.load_file('resistor_calculator.kv')
         self.resistor_3_values = RESISTOR_3_VALUES
-        self.current_resistor_3_value = self.resistor_3_values[0]
+        self.colour_to_rgba = COLOUR_TO_RGBA
         return self.root
 
-    def calculate_resistance(self, res):
-        pass
+    def return_rgba(self, colour_name):
+        try:
+            if self.root.ids.r3b1.text != '' and self.root.ids.r3b2.text != '' and self.root.ids.r3b3.text != '':
+                self.calculate_resistance()
+            return COLOUR_TO_RGBA[colour_name]
+        except KeyError:
+            return f"{colour_name} is not a key in the 'colour_to_rgba' dictionary."
+
+    def calculate_resistance(self):
+        self.root.ids.r3_value.text = "HELLO!"
 
     def display_resistor_colours(self, resistance):
         pass
